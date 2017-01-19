a2bf009cc2e5167fdbfe3e86d01c886c73880474
mhally <mhally@uvm.edu>
2017-01-18 19:06:30 -0500

    Add jquery, service for sports feed requests

---
+++ src/mySportsFeedsService.js
@@ -0,0 +1,17 @@
+$.ajax({
+   url: url,
+   type: 'GET',
+   dataType: 'json',
+   async: false,
+   headers: {
+     Authorization: 'Basic ' + btoa(username':'password)
+   },
+   success: function(data) {
+       console.log(data);
+   },
+   error : function(request, error)
+   {
+       console.log('Request: ' + JSON.stringify(request));
+       console.log('Error: ' + JSON.stringify(error));
+   }
+});
